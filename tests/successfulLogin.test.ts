01807c0d69a0f3ff0cf4a233f428025540aea4ec
Costa Giannakopoulos <costag1982@gmail.com>
2022-08-07 12:26:11 +0100

    all passing good place

--- tests/successfulLogin.test.ts
+++ tests/successfulLogin.test.ts
@@ -1,8 +1,8 @@
 import test, { expect } from '../fixtures/basePages';
 import * as data from '../data/user.json';
 
-test.describe('User login tests @login', () => {
-  test('Successful Login', async ({ loginPage, homePage, dashboardPage, navPage, page }) => {
+test.describe('User can succesfully login: @login', () => {
+  test('Successful Login first attempt', async ({ loginPage, homePage, dashboardPage, navPage, page }) => {
     // await test.step('Log in', async () => {
     //   // ...
     // });
@@ -18,9 +18,7 @@ test.describe('User login tests @login', () => {
     await loginPage.clickLoginBtn();
 
     await expect(navPage.eleloggedInName).toHaveText('Costa G');
-
     expect(page.url()).toContain('/home');
-
     await expect(dashboardPage.eleTeamName).toHaveText('Newcastle Jets FC');
 
     // expect(await page.screenshot({
@@ -28,20 +26,13 @@ test.describe('User login tests @login', () => {
     // })).toMatchSnapshot("letcode.png")
   });
 
-  test('Unsuccessful Login - no details input', async ({ loginPage, homePage, navPage, page }) => {
-    await homePage.visit();
-    expect(await page.title()).toBe('Hudl: We Help Teams and Athletes Win');
-
-    await navPage.clickLoginBtn();
-    expect(await page.title()).toBe('Log In - Hudl');
-
-    await loginPage.clickLoginBtn();
-
-    await expect(loginPage.loginErrorMessage).toHaveText("We didn't recognize that email and/or password.Need help?");
-    await expect(loginPage.loginBtn).toBeDisabled();
-  });
-
-  test('Unsuccessful Login - incorrect password', async ({ loginPage, homePage, navPage, page }) => {
+  test('User gets password wrong then corrects it and logs in', async ({
+    loginPage,
+    homePage,
+    navPage,
+    dashboardPage,
+    page
+  }) => {
     await homePage.visit();
     expect(await page.title()).toBe('Hudl: We Help Teams and Athletes Win');
 
@@ -54,5 +45,14 @@ test.describe('User login tests @login', () => {
 
     await expect(loginPage.loginErrorMessage).toHaveText("We didn't recognize that email and/or password.Need help?");
     await expect(loginPage.loginBtn).toBeDisabled();
+
+    await loginPage.enterPassword(data.password);
+    await expect(loginPage.loginBtn).toBeEnabled();
+    await expect(loginPage.loginErrorMessage).not.toBeVisible();
+    await loginPage.clickLoginBtn();
+
+    await expect(navPage.eleloggedInName).toHaveText('Costa G');
+    expect(page.url()).toContain('/home');
+    await expect(dashboardPage.eleTeamName).toHaveText('Newcastle Jets FC');
   });
 });
---
+++ tests/unsuccessfulLogin.test.ts
@@ -0,0 +1,53 @@
+import test, { expect } from '../fixtures/basePages';
+import * as data from '../data/user.json';
+import { faker } from '@faker-js/faker';
+
+test.describe('User fails to login with invalid credentials: @login', () => {
+  test('No details provided', async ({ loginPage, homePage, navPage, page }) => {
+    await homePage.visit();
+    expect(await page.title()).toBe('Hudl: We Help Teams and Athletes Win');
+
+    await navPage.clickLoginBtn();
+    expect(await page.title()).toBe('Log In - Hudl');
+
+    await loginPage.clickLoginBtn();
+
+    await expect(loginPage.loginErrorMessage).toHaveText("We didn't recognize that email and/or password.Need help?");
+    await expect(loginPage.loginBtn).toBeDisabled();
+  });
+
+  test('Incorrect password', async ({ loginPage, homePage, navPage, page }) => {
+    await homePage.visit();
+    expect(await page.title()).toBe('Hudl: We Help Teams and Athletes Win');
+
+    await navPage.clickLoginBtn();
+    expect(await page.title()).toBe('Log In - Hudl');
+
+    await loginPage.enterUsername(data.email);
+    await loginPage.enterPassword('test1234');
+    await loginPage.clickLoginBtn();
+
+    await expect(loginPage.loginErrorMessage).toHaveText("We didn't recognize that email and/or password.Need help?");
+    await expect(loginPage.loginBtn).toBeDisabled();
+  });
+
+  test('User sees different message after 7 failed attempts', async ({ loginPage, homePage, navPage, page }) => {
+    await homePage.visit();
+    expect(await page.title()).toBe('Hudl: We Help Teams and Athletes Win');
+
+    await navPage.clickLoginBtn();
+    expect(await page.title()).toBe('Log In - Hudl');
+
+    for (let attempt = 1; attempt <= 7; attempt++) {
+      const expectedMessage =
+        attempt < 7
+          ? "We didn't recognize that email and/or password.Need help?"
+          : 'Can’t remember? Don’t stress.Need help?';
+      await loginPage.enterUsername(data.email);
+      await loginPage.enterPassword(faker.internet.password());
+      await loginPage.clickLoginBtn();
+      await expect(loginPage.loginErrorMessage).toHaveText(expectedMessage);
+      await expect(loginPage.loginBtn).toBeDisabled();
+    };
+  });
+});
